f8c7f60eae6df2898e66b2ca48395182a6949555
Clayton Jones <clayton.jones@students.sacs.k12.in.us>
2023-01-18 13:54:43 -0500

    made changes to my liking

--- template.py
+++ template.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python2
-
-import pygame
+import pygame as pg
 import random
 
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+WIDTH = 1000
+HEIGHT = 1000
+FPS = 60
 
 # Define Colors 
 WHITE = (255, 255, 255)
@@ -14,47 +12,28 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+COLORS = [WHITE, BLACK, RED, GREEN, BLUE]
 
-## initialize pygame and create window
-pygame.init()
-pygame.mixer.init()  ## For sound
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("<Your game>")
-clock = pygame.time.Clock()     ## For syncing the FPS
-
-
-## group all the sprites together for ease of update
-all_sprites = pygame.sprite.group()
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("pygame template")
+clock = pg.time.Clock()
 
 ## Game loop
 running = True
 while running:
 
-    #1 Process input/events
-    clock.tick(FPS)     ## will make the loop run at the same speed all the time
-    for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
-        ## listening for the the X button at the top
-        if event.type == pygame.QUIT:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-
-
-    #2 Update
-    all_sprites.update()
-
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                running = False
 
     #3 Draw/render
     screen.fill(BLACK)
 
-    
+    pg.display.flip()       
 
-    all_sprites.draw(screen)
-    ########################
-
-    ### Your code comes here
-
-    ########################
-
-    ## Done after drawing everything to the screen
-    pygame.display.flip()       
-
-pygame.quit()
+pg.quit()
